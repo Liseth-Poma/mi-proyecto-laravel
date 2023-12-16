8a9184adb773db7b2c6cf448f23abe8bd1c2c125
Liseth-Poma <lcpoma@espe.edu.ec>
2023-12-16 09:14:51 -0500

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\ProductController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('/products', App\Http\Controllers\ProductController::class);
